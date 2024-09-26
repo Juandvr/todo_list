274b1efc85687339b8b1820036836cc018383330
Juandvr <villabonajuan203@gmail.com>
2024-09-26 18:17:57 -0500

    create function to add tasks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import { Task, Project } from './classes.js';
+import { displayProject } from './mainpage.js';
 
 const todos = new Project('Todos', 'todos');
 
+function createTask(title, desc, dueDate, priority){
+    const task = new Task(title, desc, dueDate, priority);
+    todos.tasks.push(task);
+}
+
+createTask('crear', 'proyecto', '20/23/56', 'alta');
+
+for (const task of todos.tasks){
+    displayProject(task);
+}
