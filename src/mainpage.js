75a0eff3cd8cf8e69f43a804c509bcae59b8d458
Juandvr <villabonajuan203@gmail.com>
2024-09-26 18:18:10 -0500

    create mainpage.js

---
+++ src/mainpage.js
@@ -0,0 +1,25 @@
+const projects = document.getElementById('projects');
+
+function displayProject(task){
+    const div = document.createElement('div');
+    const title = document.createElement('h3');
+    const desc = document.createElement('p');
+    const dueDate = document.createElement('p');
+    const priority = document.createElement('p');
+
+    title.innerHTML = task.title;
+    desc.innerHTML = task.desc;
+    dueDate.innerHTML = task.dueDate;
+    priority.innerHTML = task.priority;
+
+    div.appendChild(title);
+    div.appendChild(desc);
+    div.appendChild(dueDate);
+    div.appendChild(priority);
+
+    projects.appendChild(div);
+}
+
+
+
+export { displayProject };
